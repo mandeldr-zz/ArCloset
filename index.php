be5368eb5905ca335025323754060e33fbbf4b4b
Daniel Mandel <mandeldr@mcmaster.ca>
2018-01-25 19:46:22 -0500

    Added create avatar service

--- index.php
+++ index.php
@@ -26,11 +26,35 @@ $app->post('/createUser', function($request, $response, $args) {
     }
 });
 
+$app->post('/createAvatar', function($request, $response, $args) {
+    $gender = $request->getParsedBodyParam('gender');
+    $height = $request->getParsedBodyParam('height');
+    $skinColor = $request->getParsedBodyParam('skinColor');
+    $hairColor = $request->getParsedBodyParam('hairColor');
+    $hairLength = $request->getParsedBodyParam('hairLength');
+    $apiKey = $request->getParsedBodyParam('apiKey');
+
+    $db = new DbOperation();
+    $success = $db->createAvatar($gender, $height, $skinColor, $hairColor, $hairLength, $apiKey);
+
+    if($success == 0){
+        $data = array('error' => false, 'message' => 'Avatar created successfully');
+        return $response->withJson($data, 201);
+    }
+    elseif ($success == 1){
+        $data = array('error' => true, 'message' => 'Failed to create avatar');
+        return $response->withJson($data, 400);
+    }
+    else {
+        $data = array('error' => true, 'message' => 'Avatar already exists');
+        return $response->withJson($data, 400);
+    }
+});
+
 $app->get('/login', function ($request, $response, $args) {
     $username = $request->getHeaderLine('username');
     $password = $request->getHeaderLine('password');
-    echo $username;
-    echo $password;
+
     $db = new DbOperation();
     $success = $db->userLogin($username, $password);
 
