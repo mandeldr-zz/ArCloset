a1aed37c802e267a6a8590b2c4d399cf3e26f1d3
Ren-David Dimen <rendaviddimen@gmail.com>
2018-03-20 21:28:17 -0400

    update constants.php file

--- include/Constants.php
+++ include/Constants.php
@@ -1,6 +1,6 @@
 <?php
 //Constants to connect with the database
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'capstone@db-arcloset');
+define('DB_PASSWORD', '#cap2018');
+define('DB_HOST', 'db-arcloset.mysql.database.azure.com');
 define('DB_NAME', 'db_arcloset');
